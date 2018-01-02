e1f48ddd573ec1cbabffac0b2dc5a0ac4685a720
Malte Schmitz <m@mlte.de>
2018-01-02 18:03:24 +0100

    Add serial interface

--- morse.rb
+++ morse.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'serialport'
 
 class Morse < Gosu::Window
   SAMPLE_FREQUENCY = 440
@@ -16,6 +17,7 @@ class Morse < Gosu::Window
     @frequency = 660
     @history = []
     @last_time = Gosu::milliseconds()
+    @mutex = Mutex.new
   end
 
   def update
@@ -55,22 +57,50 @@ class Morse < Gosu::Window
   end
 
   def start_tone
-    unless @channel and @channel.playing?
-      volume = 1
-      speed = @frequency * 1.0 / SAMPLE_FREQUENCY
-      looping = true
-      @channel = @sample.play(volume, speed, looping)
+    @mutex.synchronize do
+      unless @channel and @channel.playing?
+        volume = 1
+        speed = @frequency * 1.0 / SAMPLE_FREQUENCY
+        looping = true
+        @channel = @sample.play(volume, speed, looping)
+      end
+      @down = true
+      @history << {width: 0, pos: 0}
     end
-    @down = true
-    @history << {width: 0, pos: 0}
   end
 
   def stop_tone
-    if @channel.playing?
-      @channel.stop
+    @mutex.synchronize do
+      if @channel and @channel.playing?
+        @channel.stop
+      end
+      @down = false
     end
-    @down = false
   end
 end
 
-Morse.new.show
+$morse = Morse.new
+
+LEFT_COMMAND = 65
+RIGHT_COMMAND = 70
+DOWN_COMMAND = 0
+UP_COMMAND = 1
+SERIAL_PORT = '/dev/cu.usbmodem1421'
+SERIAL_BAUD = 115200
+
+Thread.abort_on_exception=true
+serialThread = Thread.new do
+  port = SerialPort.new(SERIAL_PORT, SERIAL_BAUD)
+  loop do
+    c = port.getbyte
+    case c
+    when LEFT_COMMAND + UP_COMMAND
+      $morse.stop_tone
+    when LEFT_COMMAND + DOWN_COMMAND
+      $morse.start_tone
+    end
+  end
+end
+
+$morse.show
+serialThread.exit
